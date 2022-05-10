97ce75681a963d207e5b4bae14c5913fefb77c07
Lil-boss <rshaon09@gmail.com>
2022-05-10 21:45:19 +0600

    navbar complete

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from './Banner';
+
+
+const Home = () => {
+    return (
+        <div className='px-10'>
+            <Banner />
+        </div>
+    );
+};
+
+export default Home;
