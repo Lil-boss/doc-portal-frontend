cab37519a6dead0d1dc895b21ce4f2253eea7b29
Lil-boss <rshaon09@gmail.com>
2022-05-10 21:47:34 +0600

    banner complete

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                 <div>
                     <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <button class="btn bg-gradient-to-r from-primary to-secondary border-none">Get Started</button>
                 </div>
             </div>
         </div>
